2d3ba941a57cf7569b1397acb3438c820297a186
Flávia Santana Goncalves Feitosa <flaviasantana_fisio@hotmail.com>
2023-10-30 22:10:15 -0300

    nivel1-jogo da velha

---
+++ jogos/app-velha.js
@@ -0,0 +1,15 @@
+
+var jogador1 = document.createElement('img')
+jogador1.src = 'imagens/x.png'
+jogador1.className = 'img-j'
+var jogador2 = document.createElement('img')
+jogador2.src = 'imagens/o.png'
+jogador2.className = 'img-j'
+
+//https://www.youtube.com/watch?v=ohl1tK-azxw
+
+const mensagem = document.querySelector(".mensagem");
+
+let selected;
+let player = "X";
+
